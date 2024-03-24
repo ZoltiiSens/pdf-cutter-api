20017389f8dba0686b0495ff6b3d1fdffaad959e
ZoltiiSens <zoltiisen@gmail.com>
2024-03-25 00:31:51 +0200

    Added pdf->zip-images and doc/x->zip-images

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, File, Form, Response, Request, HTTPException, Depends
+from fastapi import FastAPI, File, Form, Response, Request, HTTPException, Depends, UploadFile
 from typing import Annotated
 from time import time
 from PyPDF2 import PdfReader, PdfWriter
@@ -8,12 +8,31 @@ from starlette.responses import FileResponse
 from starlette.background import BackgroundTask
 from starlette.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 from googleapiclient.errors import HttpError
+import pypdfium2 as pdfium
 
 from tools import ImageReader, cleanup, zip_files, doc_docx_to_pdf, service, get_all_text_from_pages
 from auth.auth_handler import HTTPSpecialBearer
+
 app = FastAPI()
 WEBHOOK_URL = 'https://google.com'
-imageReader = ImageReader('WINDOWS')
+imageReader = ImageReader('LINUX')
+
+
+# from typing import List
+#
+#
+# @app.post('test')
+# def add_images(images: List[UploadFile] = File(...)):
+#     for image in images:
+#         print(image)
+#     return {"msg": "success"}
+#
+#
+# @app.post('test')
+# def add_images(images: List[Annotated[bytes, File()]] = File(...)):
+#     for image in images:
+#         print(image)
+#     return {"msg": "success"}
 
 
 @app.post('/pdf/cut', dependencies=[Depends(HTTPSpecialBearer())], tags=['pdf'])
@@ -164,6 +183,54 @@ def pdf_get_all_text(file: Annotated[bytes, File()], response: Response):
     return {'text': text}
 
 
+@app.post('/pdf/convert_to_zip_images', dependencies=[Depends(HTTPSpecialBearer())], tags=['pdf'])
+def convert_pdf_to_zip_images(file: Annotated[bytes, File()], img_extension: Annotated[str, Form()],
+                                    response: Response,
+                                    width: Annotated[int, File()] = None, height: Annotated[int, File()] = None):
+    if (width is None and height is None) or (width is not None and height is not None):
+        response.status_code = HTTP_400_BAD_REQUEST
+        return {'error': 'Set one: width or height'}
+    supported_image_types = ['jpg', 'png', 'jpeg']
+    if img_extension not in supported_image_types:
+        response.status_code = HTTP_400_BAD_REQUEST
+        return {'error': 'Unsupported image type'}
+    filename = f'tmp_{time()}_{len(os.listdir("."))}'
+    pdf_filename = f'{filename}.pdf'
+    with open(pdf_filename, 'wb') as f:
+        f.write(file)
+    try:
+        filePDF = pdfium.PdfDocument(pdf_filename)
+    except pdfium._helpers.misc.PdfiumError as error:
+        return {'error': error}
+    images_filenames = []
+    for page_number in range(len(filePDF)):
+        page = filePDF.get_page(page_number)
+        pil_image = page.render(scale=1).to_pil()
+        old_width, old_height = pil_image.size
+        new_width, new_height = 0, 0
+        if width is not None:
+            new_width = width
+            new_height = int(old_height * width / old_width)
+        if height is not None:
+            new_width = int(old_width * height / old_height)
+            new_height = height
+        print(f"old:{old_width},{old_height}")
+        print(f'new:{new_width},{new_height}')
+        pil_image = pil_image.resize((new_width, new_height))
+        pil_image.save(f"{filename}_{page_number + 1}.{img_extension}")
+        images_filenames.append(f"{filename}_{page_number + 1}.{img_extension}")
+    filePDF.close()
+    zip_filename = zip_files(images_filenames, filename)
+    cleanup(pdf_filename, *images_filenames)
+    response = FileResponse(
+        zip_filename,
+        media_type="application/x-zip-compressed",
+        headers={'Content-Disposition': f'attachment; filename="{zip_filename}"'},
+        background=BackgroundTask(cleanup, zip_filename)
+    )
+    return response
+
+
 @app.post('/doc/convert_to_pdf', dependencies=[Depends(HTTPSpecialBearer())], tags=['docs'])
 def convert_doc_or_docx_to_pdf(file: Annotated[bytes, File()], extension: Annotated[str, Form()], response: Response):
     if extension != 'doc' and extension != 'docx':
@@ -269,6 +336,63 @@ def doc_get_all_text(file: Annotated[bytes, File()], extension: Annotated[str, F
     return {'text': text}
 
 
+@app.post('/doc/convert_to_zip_images', dependencies=[Depends(HTTPSpecialBearer())], tags=['docs'])
+def convert_doc_to_zip_images(file: Annotated[bytes, File()], img_extension: Annotated[str, Form()],
+                                    doc_extension: Annotated[str, Form()], response: Response,
+                                    width: Annotated[int, File()] = None, height: Annotated[int, File()] = None):
+    if doc_extension not in ['doc', 'docx']:
+        response.status_code = HTTP_400_BAD_REQUEST
+        return {'error': 'Unsupported extension: write "doc" or "docx"'}
+    if (width is None and height is None) or (width is not None and height is not None):
+        response.status_code = HTTP_400_BAD_REQUEST
+        return {'error': 'Set one: width or height'}
+    supported_image_types = ['jpg', 'png', 'jpeg']
+    if img_extension not in supported_image_types:
+        response.status_code = HTTP_400_BAD_REQUEST
+        return {'error': 'Unsupported image type'}
+    filename = f'tmp_{time()}_{len(os.listdir("."))}'
+    doc_filename = f'{filename}.{doc_extension}'
+    pdf_filename = f'{filename}.pdf'
+    with open(doc_filename, 'wb') as f:
+        f.write(file)
+    try:
+        doc_docx_to_pdf(doc_filename, pdf_filename, service)
+        cleanup(doc_filename)
+    except HttpError as error:
+        cleanup(doc_filename, pdf_filename)
+        response.status_code = HTTP_400_BAD_REQUEST
+        return {'error': error}
+    try:
+        filePDF = pdfium.PdfDocument(pdf_filename)
+    except pdfium._helpers.misc.PdfiumError as error:
+        return {'error': error}
+    images_filenames = []
+    for page_number in range(len(filePDF)):
+        page = filePDF.get_page(page_number)
+        pil_image = page.render(scale=1).to_pil()
+        old_width, old_height = pil_image.size
+        new_width, new_height = 0, 0
+        if width is not None:
+            new_width = width
+            new_height = int(old_height * width / old_width)
+        if height is not None:
+            new_width = int(old_width * height / old_height)
+            new_height = height
+        pil_image = pil_image.resize((new_width, new_height))
+        pil_image.save(f"{filename}_{page_number + 1}.{img_extension}")
+        images_filenames.append(f"{filename}_{page_number + 1}.{img_extension}")
+    filePDF.close()
+    zip_filename = zip_files(images_filenames, filename)
+    cleanup(pdf_filename, *images_filenames)
+    response = FileResponse(
+        zip_filename,
+        media_type="application/x-zip-compressed",
+        headers={'Content-Disposition': f'attachment; filename="{zip_filename}"'},
+        background=BackgroundTask(cleanup, zip_filename)
+    )
+    return response
+
+
 @app.get('/pdf/get', dependencies=[Depends(HTTPSpecialBearer())], tags=['technical_endpoints'])
 def get_pdf_list(response: Response):
     return os.listdir('files')
@@ -299,6 +423,3 @@ def delete_pdf_by_id(fileId: str, response: Response):
     else:
         response.status_code = HTTP_400_BAD_REQUEST
         return {'error': 'invalid file id'}
-
-
-
